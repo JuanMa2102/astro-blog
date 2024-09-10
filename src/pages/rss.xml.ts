1eec0ef7542ad60ffacc48d2675d67508ef2f632
Manuel Martinez <manuelmartinez199802@gmail.com>
2024-09-09 18:35:18 -0600

    contenido e imagenes

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import rss from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
+import MarkdownIt from 'markdown-it/index.js';
+import sanitizeHtml from 'sanitize-html';
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site }) => {
 
@@ -16,11 +20,25 @@ export const GET: APIRoute = async ({ params, request, site }) => {
         site: site ?? '',
         // Array of `<item>`s in output xml
         // See "Generating items" section for examples using content collections and glob imports
-        items: blogPost.map(({data, slug} : any) => ({
+        items: blogPost.map(({data, slug, body} : any) => ({
             title: data.title,
             pubDate: data.date,
             description: data.description,
+            xmlns: {
+                media: 'http://search.yahoo.com/mrss/',
+              },
             link: `/blog/${slug}`,
+            content: sanitizeHtml(parser.render(body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+              }),
+              
+              customData: `<media:content
+                  type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+                  width="${data.image.width}"
+                  height="${data.image.height}"
+                  medium="image"
+                  url="${site + data.image.src}" />
+              `,
         })),
         // stylesheet: '/styles/rss.xsl',
         // (optional) inject custom xml
